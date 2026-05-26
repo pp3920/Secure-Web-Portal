11b96c0948472b4099f83d3e9d6a2a3bf8b935ed
Priyanka22324 <priyanka22324@gmail.com>
2026-05-26 15:14:47 -0400

    Implement GitHub OAuth strategy with Passport

---
+++ config/passport.js
@@ -0,0 +1,53 @@
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
+const User = require('../models/User');
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.GITHUB_CALLBACK_URL, // e.g., 'http://localhost:3000/api/users/auth/github/callback'
+    },
+    // This is the "verify" callback
+    async (accessToken, refreshToken, profile, done) => {
+      console.dir(profile)
+    // /*
+      try {
+        // The "profile" object contains the user's GitHub information
+        /*
+// This is commented out so I could show you it Worked... when unable to connect to MongoDB
+        const existingUser = await User.findOne({ githubId: profile.id });
+
+        if (existingUser) {
+          // If user already exists, pass them to the next middleware
+          return done(null, existingUser);
+        }
+
+        // If it's a new user, create a record in our database
+        */
+        const newUser = new User({
+          githubId: profile.id,
+          username: profile.username,
+          email: "test@gmail.com", // Some providers return an array of emails
+        });
+
+        // await newUser.save();
+        done(null, newUser);
+      } catch (err) {
+        done(err);
+      }
+    // */
+
+    }
+  )
+);
+
+// These functions are needed for session management
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => done(err, user));
+});
